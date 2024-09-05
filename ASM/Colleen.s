5f200b1d889b3cd33f7c747e950f736447816982
SolDavid_76 <david.janusz75@gmail.com>
2024-09-05 15:54:07 +0200

    good advancment in asm

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -4,15 +4,15 @@ extern dprintf
 
 ;colleen
 section .data
-	payload db "bits 64%2$c%2$cextern dprintf%2$c%2$c;colleen%2$csection .data%2$c%3$cpayload db %4$c%1$s%4$c, 0%2$c%2$csection .text%2$c%3$cglobal _start%2$c%2$c%3$ccolleen:%2$c%3$c%3$cmov rdi, 1%2$c%3$c%3$clea rsi, [payload]%2$c%3$c%3$clea rdx, [payload]%2$c%3$c%3$cmov rcx, 10%2$c%3$c%3$cmov r8, 9%2$c%3$c%3$cmov r9, 34%2$c%2$c%3$c%3$cpush rbx%2$c%3$c%3$ccall dprintf%2$c%3$c%3$cpop rbx%2$c%2$c%3$c%3$cmov rax, 0%2$c%3$c%3$cret%2$c%2$c%3$c_start:%2$c%3$c%3$ccall colleen%2$c%2$c%3$c%3$cmov rax, 60%2$c%3$c%3$cmov rdi, 0%2$c%3$c%3$csyscall ; exit(0)%2$c", 0
+	payload db "bits 64%2$c%2$cextern dprintf%2$c%2$c;colleen%2$csection .data%2$c%3$cpayload db %4$c%1$s%4$c, 0%2$c%2$csection .text%2$c%3$cglobal main%2$c%2$c%3$ccolleen:%2$c%3$c%3$cmov rdi, 1%2$c%3$c%3$cmov rsi, payload%2$c%3$c%3$cmov rdx, payload%2$c%3$c%3$cmov rcx, 10%2$c%3$c%3$cmov r8, 9%2$c%3$c%3$cmov r9, 34%2$c%2$c%3$c%3$cpush rbx%2$c%3$c%3$ccall dprintf%2$c%3$c%3$cpop rbx%2$c%2$c%3$c%3$cmov rax, 0%2$c%3$c%3$cret%2$c%2$c%3$cmain:%2$c%3$c%3$cpush rbx%2$c%3$c%3$ccall colleen%2$c%3$c%3$cpop rbx%2$c%2$c%3$c%3$cmov rax, 0%2$c%3$c%3$cret ; return(0)%2$c", 0
 
 section .text
-	global _start
+	global main
 
 	colleen:
 		mov rdi, 1
-		lea rsi, [payload]
-		lea rdx, [payload]
+		mov rsi, payload
+		mov rdx, payload
 		mov rcx, 10
 		mov r8, 9
 		mov r9, 34
@@ -24,9 +24,10 @@ section .text
 		mov rax, 0
 		ret
 
-	_start:
+	main:
+		push rbx
 		call colleen
+		pop rbx
 
-		mov rax, 60
-		mov rdi, 0
-		syscall ; exit(0)
+		mov rax, 0
+		ret ; return(0)
--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,16 +1,17 @@
 bits 64
 
-extern fprintf, fputs, fopen, fclose, stderr
+extern fopen, fprintf, fclose
 
 section .data
 	fileName db "Grace_kid.s", 0
 	openMode db "w", 0
-	payload db "bits 64%2$c%2$cextern open, dprintf, close%2$c%2$csection .data%2$c%3$cfileName db %4$cGrace_kid.c%4$c, 0%2$c%3$cpayload db %4$c%1$c%4$c, 0%2$c", 0
+	payload db "bits 64%2$c%2$cextern fopen, fprintf, fclose%2$c%2$csection .data%2$c%3$cfileName db %4$cGrace_kid.s%4$c, 0%2$c%3$copenMode db %4$cw%4$c, 0%2$c%3$cpayload db %4$c%1$s%4$c, 0%2$c%2$c%%macro MAIN 0%2$csection .text%2$c%3$cglobal main%2$c%2$c%3$cmain:%2$c%3$c%3$cmov rdi, fileName%2$c%3$c%3$cmov rsi, openMode%2$c%2$c%3$c%3$cpush rbx%2$c%3$c%3$ccall fopen%2$c%3$c%3$cpop rbx%2$c%2$c%3$c%3$cpush rax%2$c%3$c%3$cmov rdi, rax%2$c%3$c%3$cmov rsi, payload%2$c%3$c%3$cmov rdx, payload%2$c%3$c%3$cmov rcx, 10%2$c%3$c%3$cmov r8, 9%2$c%3$c%3$cmov r9, 34%2$c%2$c%3$c%3$ccall fprintf ; fprintf(file, payload, payload, 10, 9, 34)%2$c%3$c%3$cpop rdi%2$c%2$c%3$c%3$cpush rbx%2$c%3$c%3$ccall fclose%2$c%3$c%3$cpop rbx%2$c%2$c%3$c%3$cmov rax, 0%2$c%3$c%3$cret%2$c%%endmacro%2$c%2$cMAIN%2$c", 0
 
+%macro MAIN 0
 section .text
-	global _start
+	global main
 
-	_start:
+	main:
 		mov rdi, fileName
 		mov rsi, openMode
 
@@ -26,35 +27,15 @@ section .text
 		mov r8, 9
 		mov r9, 34
 
-		push rbx
-		call fprintf
-		pop rbx
+		call fprintf ; fprintf(file, payload, payload, 10, 9, 34)
 		pop rdi
 
 		push rbx
 		call fclose
 		pop rbx
 
-		; mov rax, 2
-		; mov rdi, fileName
-		; mov rsi, 65
-		; mov rdx, 0666
-		; syscall
+		mov rax, 0
+		ret
+%endmacro
 
-		; mov rdi, rax
-		; mov rsi, payload
-		; mov rdx, payload
-		; mov rcx, 10
-		; mov r8, 9
-		; mov r9, 34
-
-		; push rbx
-		; call dprintf
-		; pop rbx
-
-		; mov rax, 3
-		; syscall
-
-		mov rax, 60
-		mov rdi, 0
-		syscall
+MAIN
